02bc9e2ad0095144b9aed23662de18dc35b21f32
Camila <camila.medeiros.silva2013@gmail.com>
2025-03-27 18:05:15 +0100

    feat(details): create dynamic metada to the game details page and create the static profile page.

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -4,6 +4,54 @@ import Image from 'next/image';
 import { Container } from '@/components/container';
 import { Label } from './components/label';
 import { GameCard } from '@/components/GameCard';
+import { Metadata } from 'next';
+
+interface PropsParams {
+  params: {
+    id: string;
+  };
+}
+export async function generateMetadata({
+  params,
+}: PropsParams): Promise<Metadata> {
+  try {
+    const response: GameProps = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`,
+      {
+        next: { revalidate: 60 },
+      }
+    )
+      .then(res => res.json())
+      .catch(() => {
+        return {
+          title: 'Daily games find incredible games.',
+        };
+      });
+
+    return {
+      title: response.title,
+      description: `${response.description.slice(0, 150)}...`,
+      openGraph: {
+        title: response.title,
+        images: [response.image_url],
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
+        },
+      },
+    };
+  } catch (e) {
+    return {
+      title: 'Daily games find incredible games.',
+    };
+  }
+}
 
 async function getData(id: string) {
   try {
